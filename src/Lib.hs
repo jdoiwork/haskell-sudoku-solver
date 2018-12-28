b83bc5223381bd1fe1db6e78bf1e3e5bc8ea5794
jdoi <jdoi.work@gmail.com>
2018-12-28 22:43:25 +0900

    難しそうな問題を追加

--- src/Lib.hs
+++ src/Lib.hs
@@ -128,6 +128,37 @@ sampleSrc = [ "5176   34" -- 1
             , "         " -- 9
             ]
 
+sampleSrc2 :: [String]
+sampleSrc2 =
+    [ "850002400" -- 1
+    , "720000009" -- 2
+    , "004000000" -- 3
+    , "000107002" -- 4
+    , "305000900" -- 5
+    , "040000000" -- 6
+    , "000080070" -- 7
+    , "017000000" -- 8
+    , "000036040" -- 9
+    ]
+
+sampleSrc3 :: [String]
+sampleSrc3 =
+    [ "370001000" -- 1
+    , "000902000" -- 2
+    , "000006510" -- 3
+    , "002000640" -- 4
+    , "190000000" -- 5
+    , "040009020" -- 6
+    , "004000150" -- 7
+    , "000800000" -- 8
+    , "600500700" -- 9
+    ]
+
 sampleBoard :: Board
 sampleBoard = parseBoard $ unlines sampleSrc
 
+sampleBoard2 :: Board
+sampleBoard2 = parseBoard $ unlines sampleSrc2
+
+sampleBoard3 :: Board
+sampleBoard3 = parseBoard $ unlines sampleSrc3
